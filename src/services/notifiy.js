2155c6ddf5f4d8eee988649a8fa3a31f63bcf161
elegantYU <845384559@qq.com>
2020-08-31 14:11:05 +0800

    fix: 修复交易消息通知bug

--- src/services/notifiy.js
+++ src/services/notifiy.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-08-11 15:18:45
  * @LastEditors: elegantYu
- * @LastEditTime: 2020-08-30 10:05:21
+ * @LastEditTime: 2020-08-31 14:10:30
  * @Description: 插件的所有通知
  */
 import { createNotifiy, clearNotifiy, getURL } from "./base";
@@ -10,26 +10,29 @@ import { getLocal } from "../App/popup/services/localStorage";
 import Constants from "../constants";
 
 const LOGO = getURL("./static/icons/logo128.png");
-const localConfig = getLocal(Constants.LOCAL_CONFIG);
-const isOpen = localConfig && localConfig["notify"];
+const checkNotifyOpen = () => {
+	const localConfig = getLocal(Constants.LOCAL_CONFIG);
+	const isOpen = localConfig && localConfig["notify"];
+	return isOpen;
+};
 
-isOpen &&
-	requestRecursion({
-		fns: arrivalRemind,
-		check: () => false,
-		time: 60000,
-		callback: (isArrival) => {
-			isArrival &&
-				createNotifiy(Constants.REMIND_MARKET_CLOSE, {
-					type: "basic",
-					title: "基金管理助手",
-					iconUrl: LOGO,
-					message: "还剩5分钟今日交易结束",
-					priority: 2,
-				}).then((_) => {
-					setTimeout(() => {
-						clearNotifiy(Constants.REMIND_MARKET_CLOSE);
-					}, 120000);
-				});
-		},
-	});
+requestRecursion({
+	fns: [arrivalRemind, checkNotifyOpen],
+	check: () => false,
+	time: 60000,
+	callback: ([isArrival, notifyOpen]) => {
+		isArrival &&
+			notifyOpen &&
+			createNotifiy(Constants.REMIND_MARKET_CLOSE, {
+				type: "basic",
+				title: "基金管理助手",
+				iconUrl: LOGO,
+				message: "还剩5分钟今日交易结束",
+				priority: 2,
+			}).then((_) => {
+				setTimeout(() => {
+					clearNotifiy(Constants.REMIND_MARKET_CLOSE);
+				}, 120000);
+			});
+	},
+});
